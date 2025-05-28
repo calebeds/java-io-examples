12ba0c6d8d38497641daac07bedc12d45e8e2677
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-05-28 20:03:24 -0300

    add pushback stream example

---
+++ src/main/java/bytestreams/PushBackStreams.java
@@ -0,0 +1,33 @@
+package bytestreams;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PushbackInputStream;
+
+public class PushBackStreams {
+    public static void main(String[] args) throws IOException {
+        String s = "if(a && b) return c & d";
+
+        byte[] buffer = s.getBytes();
+
+        InputStream in = new ByteArrayInputStream(buffer);
+        PushbackInputStream filter = new PushbackInputStream(in);
+
+        int b = 0;
+        while ((b = filter.read()) != -1) {
+            switch (b) {
+                case '&':
+                    if((b = filter.read()) == '&') {
+                        System.out.print("Logical And");
+                    } else {
+                        System.out.print("Bitwise And");
+                        filter.unread(b);
+                    }
+                    break;
+                default:
+                    System.out.print((char) b);
+            }
+        }
+    }
+}
