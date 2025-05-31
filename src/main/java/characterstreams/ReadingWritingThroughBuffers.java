3c0f2ccb12bef1c9cd9452930ce0569bb28e99c2
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-05-31 15:34:35 -0300

    add reading and writing through buffer example

---
+++ src/main/java/characterstreams/ReadingWritingThroughBuffers.java
@@ -0,0 +1,28 @@
+package characterstreams;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+
+public class ReadingWritingThroughBuffers {
+    public static void main(String[] args) throws IOException {
+        File file = new File("src/Frankestein.txt");
+        File outFile = new File("src/FrankesteinCopy.txt");
+
+        Reader reader = new FileReader(file);
+        Writer writer = new FileWriter(outFile);
+        char[] buffer = new char[1024];
+
+        int charRead = 0;
+
+        while (reader.read(buffer) != -1) {
+            writer.write(buffer);
+        }
+
+        reader.close();
+        reader.close();
+    }
+}
