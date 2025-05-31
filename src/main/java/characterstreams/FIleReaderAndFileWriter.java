169ec5e32f499232656e73dd99c5e40114b7dff7
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-05-31 15:10:38 -0300

    add file reader and file writer character by character example

---
+++ src/main/java/characterstreams/FIleReaderAndFileWriter.java
@@ -0,0 +1,39 @@
+package characterstreams;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class FIleReaderAndFileWriter {
+    public static void main(String[] args) {
+        File file = new File("src/Frankestein.txt");
+        File bookInformation = new File("src/BookInformation.txt");
+
+        try(Reader reader = new FileReader(file, StandardCharsets.UTF_8); Writer writer = new FileWriter(bookInformation)) {
+//        System.out.println(Charset.defaultCharset());
+            bookInformation.createNewFile();
+
+            int charRead = 0;
+            int starRead = 0;
+            while ((charRead = reader.read()) != -1) {
+                if(starRead == 3) {
+                    break;
+                }
+
+                if(((char) charRead) == '*') {
+                    starRead++;
+                }
+
+                writer.write((char) charRead);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
