738437064862e8db3ea3b95a940fff2aafe8dfd1
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-05-31 16:12:56 -0300

    add skipping example on File reader and writer

---
+++ src/main/java/characterstreams/SkippingMarkingResetting.java
@@ -0,0 +1,27 @@
+package characterstreams;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+
+public class SkippingMarkingResetting {
+    public static void main(String[] args) {
+        File file = new File("src/Frankestein.txt");
+        File outFile = new File("src/BookContent");
+
+        try(Reader reader = new FileReader(file); Writer writer = new FileWriter(outFile)) {
+            outFile.createNewFile();
+            reader.skip(904);
+            int charRead = 0;
+            while ((charRead = reader.read()) != -1) {
+                writer.write((char) charRead);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
